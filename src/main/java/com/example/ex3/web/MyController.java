3ab7ce6ccc1d85f960eed6a8283dee8a804052e8
niaday6 <ekdud9506@naver.com>
2023-09-18 10:52:27 +0900

    /main 요청 처리

---
+++ src/main/java/com/example/ex3/web/MyController.java
@@ -0,0 +1,19 @@
+package com.example.ex3.web;
+
+import lombok.extern.java.Log;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@Log4j2
+public class MyController {
+
+    @GetMapping("/main")
+    public void main(){
+       log.info("==========================main");
+    }
+
+
+
+}
